41aeee88e06db628d8edcbbed69a1b44228baab4
Alejandro Flores <sjafs.83@gmail.com>
2021-10-01 01:22:11 -0300

    first commit

---
+++ src/componets/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <>
+            <nav>
+                <div className="logo"> ALE FLORES</div>
+            </nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#home">About</a></li>
+                <li><a href="#home">Projects</a></li>
+                <li><a href="#home">Contact Me</a></li>
+                <li><a href="#home">My Personal CV</a></li>
+            </ul>
+        </>
+    )
+}
